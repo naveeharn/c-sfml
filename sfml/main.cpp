6eaabde6bc580873cd0f3579d9d7f702b976ef9f
naveeharn <naveeharn@hotmail.com>
2020-09-14 20:28:50 +0700

    test

---
+++ sfml/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sfml
+//
+//  Created by Naveeharn Tehmarn on 14/9/2563 BE.
+//  Copyright © 2563 Naveeharn Tehmarn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
